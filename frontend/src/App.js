fda5b380f726a057e3b82a18b7a77fa6add861d5
kundlatsch <gustavo.kundlatsch@gmail.com>
2023-07-14 21:18:12 -0300

    Add booklist page structure

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
+import BookList from './pages/BookList';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Hello</h1>
+        <BookList></BookList>
       </header>
     </div>
   );
---
+++ frontend/src/pages/BookList/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './styles.css';
+
+
+function BookList() {
+
+  return (
+    <div>
+      <h1>BookList</h1>
+    </div>
+  )
+}
+
+export default BookList
